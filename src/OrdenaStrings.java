32925e28e0a40bf83e49856bba9d7ed14080f1fc
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-06 08:59:57 -0300

    Usando o método Collectiosn.sort, que usa o critério de comparação da classe String - ordem natural

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+		
+		Collections.sort(palavras);
+		System.out.println(palavras);
 
 	}
 
