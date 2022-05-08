59195331df0153547d007eaaae77357fccbe8258
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-08 09:46:34 -0300

    OptionalDouble

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 class Curso {
@@ -81,6 +82,12 @@ public class ExemploCursos {
 		System.out.println(curso.getNome());
 		
 		optionalCurso.ifPresent(c -> System.out.println(c.getNome()));
+		
+		OptionalDouble media  = cursos.stream()
+				.filter(c -> c.getAlunos() >= 100)
+				.mapToInt(Curso::getAlunos)
+				.average();
+		
 		}
 		
 	}
