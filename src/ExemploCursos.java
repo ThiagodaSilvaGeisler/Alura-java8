a08e1fd2441973d855aa6e62a2f055122907b0c6
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-08 08:33:59 -0300

    Map com lambda

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -1,40 +1,61 @@
+import java.net.PortUnreachableException;
+import java.nio.file.DirectoryStream.Filter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 class Curso {
-		private String nome;
-		private int alunos;
-		
-		public Curso(String nome, int alunos) {
-			this.nome = nome;
-			this.alunos = alunos;
-		}
-		
-		public String getNome() {
-			return nome;
-		}
-		
-		public int getAlunos() {
-			return alunos;
-		}
-		
+	private String nome;
+	private int alunos;
+
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getAlunos() {
+		return alunos;
+	}
+
+}
 
 public class ExemploCursos {
 	public static void main(String[] args) {
 		List<Curso> cursos = new ArrayList<Curso>();
 		cursos.add(new Curso("Python", 45));
 		cursos.add(new Curso("JavaScript", 150));
-		cursos.add(new Curso("Java 8", 133));
+		cursos.add(new Curso("Java 8", 113));
 		cursos.add(new Curso("C", 55));
-		
+
 		cursos.sort(Comparator.comparing(Curso::getAlunos));
-		
+
 		cursos.forEach(c -> System.out.println(c.getNome()));
-		
+
+		System.out.println("------------------------------------------------------------------------------");
+
+		for (Curso curso : cursos) {
+			if(curso.getAlunos() >= 100) {
+				System.out.println(curso.getNome());
+			}
+		}
+				
 		System.out.println("------------------------------------------------------------------------------");
 		
-		cursos.stream().filter(c -> c.getAlunos() >= 100).forEach(c -> System.out.println(c.getNome()));
+		cursos.stream()
+		.filter(c -> c.getAlunos() >= 100)
+		.forEach(c -> System.out.println(c.getNome()));
+
+		System.out.println("------------------------------------------------------------------------------");
+
+		cursos.stream()
+		.filter(c -> c.getAlunos() >= 100)
+		.map(c -> c.getAlunos())
+		.forEach(total -> System.out.println(total));		
+		}
+		
 	}
-}
+
